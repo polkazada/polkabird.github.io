24ea9f3f4f987fe47080cc2c360034aec9e119b0
joe-fleaux <48878069+joe-fleaux@users.noreply.github.com>
2020-05-19 05:34:45 +0800

    Update lazada.php

--- lazada.php
+++ lazada.php
@@ -1,5 +1,9 @@
-<?php
+<html><head><title>polkazada clone</title></head>
+  <body>
+    <?php
 $content = file_get_contents('https://www.lazada.com.ph');
 $content = str_replace('</title>','</title><base href="https://www.lazada.com.ph" />', $content);
 $content = str_replace('</head>','<link rel="stylesheet" href="http://polkazada.github.io/lazada.css" /></head>', $content);
-echo $content;
+echo $content; 
+  </body>
+</html>
