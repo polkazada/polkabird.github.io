33ec893331b8f25226c581358457960c9bc9988c
joe-fleaux <48878069+joe-fleaux@users.noreply.github.com>
2020-05-19 05:30:09 +0800

    Create lazada.php

---
+++ lazada.php
@@ -0,0 +1,5 @@
+<?php
+$content = file_get_contents('https://www.lazada.com.ph');
+$content = str_replace('</title>','</title><base href="https://www.lazada.com.ph" />', $content);
+$content = str_replace('</head>','<link rel="stylesheet" href="http://polkazada.github.io/lazada.css" /></head>', $content);
+echo $content;
